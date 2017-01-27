3abf8a178f3c75f2bb3c1b4d6a3c8e4e25e91418
Brunno Vodola <bvodola@gmail.com>
2017-01-27 11:43:23 -0200

    1283

--- landing/urls.py
+++ landing/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    url(r'^services/', views.services, name='services'),
+    url(r'^portfolio/', views.portfolio, name='portfolio'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^', views.home, name='home'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'landing/index.html', {})
+def home(request):
+    return render(request, 'landing/home.html', {})
+
+def services(request):
+    return render(request, 'landing/services.html', {})
+
+def portfolio(request):
+    return render(request, 'landing/portfolio.html', {})
+
+def contact(request):
+    return render(request, 'landing/contact.html', {})
